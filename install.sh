db814dded988cf7f6ab1c04db9ab46061f4b42f9
vdvz <vizir.vadim@mail.ru>
2020-05-23 18:07:22 +0300

    new2

--- install.sh
+++ install.sh
@@ -3,3 +3,5 @@ cp haproxy.cfg /etc/haproxy
 cp interfaces /etc/network/interfaces
 cp ports.conf /etc/apache2/
 cp site1.com.conf /etc/apache2/sites-available/
+cd /usr/sbin/corosync-keygen
+corosync-keygen
